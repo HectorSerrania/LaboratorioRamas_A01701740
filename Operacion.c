e8dbf9f224adacb153c96a5aa8f7ed2e1408d10a
hector escamilla <A01701740@itesm.mx>
2022-09-14 22:50:49 -0500

    Versión Participante

--- Operacion.c
+++ Operacion.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int suma(int a, int b){
-    return a+b;
-}
-int resta(int a, int b){
-    return a-b;
-}
 int main(){
 int num1 = 12;
 int num2 = 3;
@@ -15,7 +9,5 @@ printf("%i",op1);
 int op2 = resta(num1,num2);
 13
 printf("%i",op2);
-int op3 = multi(num1,num2);
-printf("%i",op3);
 return 0;
 }
